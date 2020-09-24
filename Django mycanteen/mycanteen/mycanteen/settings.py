33384c637cdc46338eecf4c7199a0eb3efb119c2
Sharanuspatil <68810656+Sharanuspatil@users.noreply.github.com>
2020-09-24 10:03:58 +0530

    Update settings.py

--- Django mycanteen/mycanteen/mycanteen/settings.py
+++ Django mycanteen/mycanteen/mycanteen/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7r*sg69t7avftqg+8_rusfq46v0plgtzyai5rd2kmfs=5q6p2r'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
